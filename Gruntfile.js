1109272eb7ee98f2d5f38401fde4e35bd0477410
James Hu <james.hu.ustc@hotmail.com>
2015-09-16 09:06:02 +1000

    don't load grunt-cli from grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,10 @@
 module.exports = function(grunt) {
 	// Loading dependencies
 	for (var key in grunt.file.readJSON("package.json").devDependencies) {
-		if (key !== "grunt" && key.indexOf("grunt") === 0) grunt.loadNpmTasks(key);
+		if (key !== "grunt" && key.indexOf("grunt") === 0	&&
+				key !== "grunt-cli" && key.indexOf("grunt-cli") === 0){
+			grunt.loadNpmTasks(key);
+		}
 	}
 
 	var gitDescribe = require('git-describe');
