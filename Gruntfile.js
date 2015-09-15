3d7d59df4181931bcd04fcb626ce41eaca2980e0
James Hu <james.hu.ustc@hotmail.com>
2015-09-16 09:10:32 +1000

    it seems that we do need to load grunt-cli

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,7 @@
 module.exports = function(grunt) {
 	// Loading dependencies
 	for (var key in grunt.file.readJSON("package.json").devDependencies) {
-		if (key !== "grunt" && key.indexOf("grunt") === 0	&&
-				key !== "grunt-cli" && key.indexOf("grunt-cli") === 0){
+		if (key !== "grunt" && key.indexOf("grunt") === 0){
 			grunt.loadNpmTasks(key);
 		}
 	}
